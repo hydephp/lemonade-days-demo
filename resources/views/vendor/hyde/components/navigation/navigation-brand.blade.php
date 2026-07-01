da6bce8e1b6354079854507dac4ad564de0a2e17
Emma De Silva <emma@desilva.se>
2026-07-01 03:18:12 +0200

    Add logo

--- resources/views/vendor/hyde/components/navigation/navigation-brand.blade.php
+++ resources/views/vendor/hyde/components/navigation/navigation-brand.blade.php
@@ -1,3 +1,4 @@
-<a href="{{ Routes::find('index') }}" class="font-serif font-bold text-2xl px-4 text-charcoal hover:bg-lemon transition-colors" aria-label="Home page">
+<a href="{{ Routes::find('index') }}" class="font-serif font-bold text-2xl px-4 text-charcoal hover:bg-lemon transition-colors flex items-center" aria-label="Home page">
+    <img src="{{ Hyde::asset('logo.webp') }}" alt="Logo" class="h-12" />
     {{ config('hyde.name', 'HydePHP') }}
 </a>
